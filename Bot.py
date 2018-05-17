ae85e9ef6e1aaa97adc7624d100d40bcff37a02d
LingRusher <spyhawk9@gmail.com>
2018-05-17 15:25:31 -0500

    Add files via upload

---
+++ Bot.py
@@ -0,0 +1,26 @@
+#LingLing bot by LingRusher and Spird
+
+import discord 
+from discord.ext import commands
+from discord.ext.commands import Bot 
+import asyncio 
+
+bot = commands.Bot(command_prefix='!')
+
+@bot.event
+async def on_ready():
+    print ("Execute order 66")
+
+@bot.command(pass_context = True)
+async def ping(ctx):
+    await bot.say(":ping_pong: ping")
+
+@bot.command(pass_context = True)
+async def ree(ctx):
+    await bot.say("ree? REEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+@bot.command(pass_context = True)
+async def rebit(ctx):
+    await bot.say("rebit? you mean REEbit MAHEEKA!?! REEBIT!")
+
+bot.run("NDQ2NzA0MTkxMjcyOTEwODU4.Dd85Pw.Sv_Cl5cBzh81AhcyMxUAR_xw3nw")
