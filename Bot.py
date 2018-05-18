cdabf3adc3d83e2d27efc26a09906b702b5575f1
LingRusher <spyhawk9@gmail.com>
2018-05-17 20:14:34 -0500

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -11,16 +11,28 @@ bot = commands.Bot(command_prefix='!')
 async def on_ready():
     print ("Execute order 66")
 
+#ping test
 @bot.command(pass_context = True)
 async def ping(ctx):
     await bot.say(":ping_pong: ping")
 
+#ree
 @bot.command(pass_context = True)
 async def ree(ctx):
     await bot.say("ree? REEEEEEEEEEEEEEEEEEEEEEEEEEE")
 
+#reebit
 @bot.command(pass_context = True)
 async def rebit(ctx):
     await bot.say("rebit? you mean REEbit MAHEEKA!?! REEBIT!")
 
-bot.run("message me for bot id")
+#stream info. *WORK IN PROGRESS*
+@bot.command(pass_context = True)
+async def streams(ctx):
+    embed = discord.Embed(title= "Mapledino Stream", description= "https://www.twitch.tv/thetitaneater", color= 0x00ff00)
+    embed.add_field(name= "LingLing Stream", value= "https://www.twitch.tv/spyhawk9", inline= True)
+    embed.add_field(name= "Dad's Stream", value= "https://www.twitch.tv/coxki", inline= True)
+    embed.set_footer(text= "")
+    await bot.say(embed=embed)
+    
+bot.run("NDQ2NzA0MTkxMjcyOTEwODU4.Dd85Pw.Sv_Cl5cBzh81AhcyMxUAR_xw3nw")
